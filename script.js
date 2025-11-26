1b01cece38f4c376f6924cdc101aa888fde8b3e8
Taha_Afzali <elfo.dp8787@gmail.com>
2025-11-27 00:46:17 +0330

    add request func

--- script.js
+++ script.js
@@ -5,5 +5,8 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.ru/rest/v3.1/name/portugal');
+request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
 request.send();
+request.addEventListener('load', function () {
+  console.log(this.responseText);
+});
