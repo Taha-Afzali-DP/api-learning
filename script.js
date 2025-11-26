ba225bd6a0cd1fb5eb8d189f720d1b92bf3dc186
Taha_Afzali <elfo.dp8787@gmail.com>
2025-11-27 00:47:12 +0330

    add request func

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+//TODO ***add data with api with XMLHttpRequest***
 const request = new XMLHttpRequest();
 request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
 request.send();
