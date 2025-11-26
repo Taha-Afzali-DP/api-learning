4e3a73696964124b38b980c2f7e23738e21eba20
Taha_Afzali <elfo.dp8787@gmail.com>
2025-11-27 00:37:37 +0330

    add request.open var

--- script.js
+++ script.js
@@ -5,3 +5,4 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.ru/rest/v3.1/name/iran');
