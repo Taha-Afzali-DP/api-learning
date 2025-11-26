1af8f285309ca3943069296b72b293790a5f7ce9
Taha_Afzali <elfo.dp8787@gmail.com>
2025-11-27 01:30:23 +0330

    add JSON.parse

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 //TODO ***add data with api with XMLHttpRequest***
+const getCountryData = function (country) {};
 const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
+request.open('GET', `https://restcountries.com/v3.1/name/country`);
 request.send();
 request.addEventListener('load', function () {
   const data = JSON.parse(this.responseText);
