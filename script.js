7e0653dca7d272e5fbd90c0d025ef16f76d4eca9
Taha_Afzali <elfo.dp8787@gmail.com>
2025-12-09 22:07:16 +0330

    new part

--- script.js
+++ script.js
@@ -3,40 +3,36 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText); // destructuring آرایه
-    console.log(data);
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText); // destructuring آرایه
+//     console.log(data);
 
-    const html = `
-      <article class="country">
-        <img class="country__img" src="${data.flags.svg}" />
-        <div class="country__data">
-          <h3 class="country__name">${data.name.common}</h3>
-          <h4 class="country__region">${data.region}</h4>
-          <p class="country__row"><span>👫</span>${(
-            +data.population / 1000000
-          ).toFixed(1)} million</p>
-          <p class="country__row"><span>🗣️</span>${
-            Object.values(data.languages)[0]
-          }</p>
-          <p class="country__row"><span>💰</span>${
-            Object.values(data.currencies)[0].name
-          }</p>
-        </div>
-      </article>`;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
-  });
-};
+//     const html = `
+//       <article class="country">
+//         <img class="country__img" src="${data.flags.svg}" />
+//         <div class="country__data">
+//           <h3 class="country__name">${data.name.common}</h3>
+//           <h4 class="country__region">${data.region}</h4>
+//           <p class="country__row"><span>👫</span>${(
+//             +data.population / 1000000
+//           ).toFixed(1)} million</p>
+//           <p class="country__row"><span>🗣️</span>${
+//             Object.values(data.languages)[0]
+//           }</p>
+//           <p class="country__row"><span>💰</span>${
+//             Object.values(data.currencies)[0].name
+//           }</p>
+//         </div>
+//       </article>`;
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
 
-// getCountryData('germany');
-getCountryData('portugal');
-// getCountryData('usa');
-getCountryData('iran');
 //TODO *** new ppart ***
 const renderCountry = function (data) {
   const html = `
@@ -64,8 +60,12 @@ const getCountryDataAndNeighbour = function (country) {
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText); // destructuring آرایه
     console.log(data);
-
+    renderCountry(data);
     countriesContainer.insertAdjacentHTML('beforeend', html);
     countriesContainer.style.opacity = 1;
   });
 };
+// getCountryData('germany');
+getCountryData('portugal');
+// getCountryData('usa');
+getCountryData('iran');
