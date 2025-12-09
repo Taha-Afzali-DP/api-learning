df7301790adb5c75127ee659c7ef05946e12af96
Taha_Afzali <elfo.dp8787@gmail.com>
2025-12-09 22:02:17 +0330

    new part

--- script.js
+++ script.js
@@ -33,7 +33,37 @@ const getCountryData = function (country) {
   });
 };
 
-getCountryData('germany');
+// getCountryData('germany');
 getCountryData('portugal');
 // getCountryData('usa');
 getCountryData('iran');
+
+const getCountryDataAndNeighbour = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText); // destructuring آرایه
+    console.log(data);
+
+    const html = `
+      <article class="country">
+        <img class="country__img" src="${data.flags.svg}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name.common}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+          ).toFixed(1)} million</p>
+          <p class="country__row"><span>🗣️</span>${
+            Object.values(data.languages)[0]
+          }</p>
+          <p class="country__row"><span>💰</span>${
+            Object.values(data.currencies)[0].name
+          }</p>
+        </div>
+      </article>`;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
