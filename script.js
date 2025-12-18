5522b76cd0be508883b05f1a5100a0158d4f9bc4
Taha_Afzali <elfo.dp8787@gmail.com>
2025-12-19 00:41:15 +0330

    Part 12

--- script.js
+++ script.js
@@ -123,7 +123,7 @@ const getCountryData = function (country) {
     //! //country 2\\
     // const neighbour = data[0].borders[0];
     const neighbour = 'fgdhdtj';
-    if (!neighbour) return;
+    if (!neighbour) throw new Error(`This Country don't have a neighbour`);
     return getJSON(
       `https://restcountries.com/v3.1/alpha/${neighbour}`,
       'Country not found'
