c3a031238b243944f76f4290f9381731dc3e002d
Taha_Afzali <elfo.dp8787@gmail.com>
2026-01-01 22:21:12 +0330

    add challange

---
+++ challange/script.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const wait = function (seconds) {
+  return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+};
+
+const imageContainer = document.querySelector('.images');
+
+function createImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+
+    img.addEventListener('load', function () {
+      imageContainer.appendChild(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error(`عکس یافت نشد: ${url}`));
+    });
+
+    img.src = url;
+    img.alt = 'Loaded image';
+  });
+}
+
+let currentImage;
+
+createImage('./img/img-1.jpg')
+  .then(img => {
+    currentImage = img;
+    console.log('عکس ۱ لود شد');
+    return wait(2);
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+    return createImage('./img/img-2.jpg');
+  })
+  .then(img => {
+    currentImage = img;
+    console.log('عکس ۲ لود شد');
+    return wait(2);
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+    console.log('تمام عکس‌ها لود و مخفی شدند');
+    return createImage('./img/img-3.jpg');
+  })
+  .then(img => {
+    currentImage = img;
+    console.log('عکس ۳ لود شد');
+  })
+  .catch(err => {
+    console.error('خطا:', err.message);
+
+    imageContainer.innerHTML = `<p style="color: red;">${err.message}</p>`;
+  });
