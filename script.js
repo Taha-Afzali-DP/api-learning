65c0dd0f5bb1061c370537f128c440ac9b1c491f
Taha_Afzali <elfo.dp8787@gmail.com>
2026-01-01 23:44:51 +0330

    part 19

--- script.js
+++ script.js
@@ -286,7 +286,7 @@ const whereAmI = async function () {
     console.log(data);
     renderCountry(data[0]);
   } catch (err) {
-    console.log(err.message);
+    renderCountry(`somthing went worng ‼️ ${err.message}`);
   }
 };
 whereAmI();
