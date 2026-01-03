53eee1277492422b95b7ba4419ad425380dfc5e1
Taha_Afzali <elfo.dp8787@gmail.com>
2026-01-03 23:02:19 +0330

    part 22

--- script.js
+++ script.js
@@ -319,9 +319,14 @@ whereAmI()
 
 const get3Countries = async function (c1, c2, c3) {
   try {
-    const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
-    const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
-    const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+    Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
     console.log([data1.capital, data2.capital, data3.capital]);
   } catch (err) {
     console.error(err);
