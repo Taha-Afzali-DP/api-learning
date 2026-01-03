ba8540b67e7897e5a43b47a746c17898d3ca1c35
Taha_Afzali <elfo.dp8787@gmail.com>
2026-01-03 23:06:48 +0330

    part 22

--- script.js
+++ script.js
@@ -327,7 +327,7 @@ const get3Countries = async function (c1, c2, c3) {
       getJSON(`https://restcountries.com/v3.1/name/${c2}`),
       getJSON(`https://restcountries.com/v3.1/name/${c3}`),
     ]);
-    console.log([data]);
+    console.log(data.map(d => d[0].capital));
   } catch (err) {
     console.error(err);
   }
