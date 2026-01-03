29f5306128d297dc480f0bd5ee67a2356ae83c09
Taha_Afzali <elfo.dp8787@gmail.com>
2026-01-03 21:46:50 +0330

    part 21

--- script.js
+++ script.js
@@ -286,6 +286,7 @@ const whereAmI = async function () {
 
     const data = await res.json();
     renderCountry(data[0]);
+    return `you are in ${dataGeo.city}`;
   } catch (err) {
     renderError(`somthing went worng ‼️ ${err.message}`);
   }
