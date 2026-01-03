ea619257dd0a0582df6c2d33e4b5b1615382c1ca
Taha_Afzali <elfo.dp8787@gmail.com>
2026-01-03 23:15:53 +0330

    part 23

--- script.js
+++ script.js
@@ -337,6 +337,8 @@ get3Countries('iran', 'iraq', 'france');
 //! Promise.race
 (async function () {
   const res = Promise.race(
-    getJSON(`https://restcountries.com/v3.1/name/italy`)
+    getJSON(`https://restcountries.com/v3.1/name/italy`),
+    getJSON(`https://restcountries.com/v3.1/name/iran`),
+    getJSON(`https://restcountries.com/v3.1/name/mexico`)
   );
 });
